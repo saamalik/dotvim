e1bb7702da0f95ad1cb866c9a349b97b04a9a464
Saad Malik <saamalik@cisco.com>
2013-07-26 19:30:48 -0700

    Updated sync-bundles

--- sync-bundles.sh
+++ sync-bundles.sh
@@ -17,7 +17,25 @@ function update_modules {
 }
 
 echo "Removing, updating and installing bundles..."
-vim +BundleClean! +BundleInstall! +qall
-echo "Updating NPM modules..."
+# vim +BundleClean! +BundleInstall! +qall
+
+# echo "Updating NPM modules..."
 #update_modules tern_for_vim
 #echo "All synced!"
+
+echo "Installing vimrc link"
+ln -sf $PWD/vimrc $HOME/.vimrc
+
+cd dotfiles
+
+path="${PWD#$HOME/}"
+echo "path $path"
+for f in *; do
+	echo "Linking file: $f"
+	ln -sf $path/$f $HOME/.$f
+done
+
+# windows nodejs can't follow symlinks
+case `uname` in
+    *CYGWIN*) echo "Copy file: jshintrc"; rm $HOME/.jshintrc; cp -f jshintrc $HOME/.jshintrc;;
+esac
